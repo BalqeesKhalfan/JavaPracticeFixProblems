5bb24fcfa3ca7e2a0e76f4f69363a3a19b217d8d
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-02-06 11:43:46 +0400

    casting integer value to float value

---
+++ UserAge.java
@@ -0,0 +1,11 @@
+class UserAge {
+       public static void main(String[] args) {
+        int userAge=26;
+        float result=(float)(userAge);
+        System.out.println(Age +result);
+        
+        
+        
+    }
+    
+}
