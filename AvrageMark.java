38c6db9f5eb4f9bf24a1c6f1fd47ba92cfd1cba9
BalqeesKhalfan <b.k.malshuraqia@gmail.com>
2024-02-06 12:29:52 +0400

    Casting integer avrage of mark into float

---
+++ AvrageMark.java
@@ -0,0 +1,9 @@
+class AvrageMark {
+    public static void main(String[] args) {
+       int firstMark=99;
+       int secondMark=87;
+       int thirdMark=90;
+       float avrage= (float) (firstMark + secondMark + thirdMark );
+       System.out.println("Avrage "+avrage);
+    }
+}
