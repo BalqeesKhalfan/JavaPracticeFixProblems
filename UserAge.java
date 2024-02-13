1007620d604e24ce6527faaf0345acfcdeea7361
BalqeesKhalfan <109449827+BalqeesKhalfan@users.noreply.github.com>
2024-02-13 09:49:17 +0400

    Update UserAge.java

--- UserAge.java
+++ UserAge.java
@@ -2,7 +2,7 @@ class UserAge {
        public static void main(String[] args) {
         int userAge=26;
         //float result=(float)(userAge);
-        System.out.println(Age +userAge);
+        System.out.println(" My Age is :- " +userAge);
         
         
         
